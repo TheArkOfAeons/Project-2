605cf518923a316137f0dc3a64ec8942902b3bf7
Haub100 <haub100@mail.chapman.edu>
2015-04-12 17:02:01 -0700

    Brent's Project 1 Scripts

---
+++ BrentsProjectCode/MenuScripts/LoadMainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMainMenu : MonoBehaviour 
+{
+	public void LoadTheMainMenu()
+	{
+		Application.LoadLevel (0);
+	}
+}
---
+++ BrentsProjectCode/MenuScripts/LoadWin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadWin : MonoBehaviour 
+{
+	public void LoadTheWin()
+	{
+		Application.LoadLevel (2);
+	}
+}
