958f6a80126ac72df3f865ee04b57504c4c76c4e
Andy de Caussin <thearkofmario@gmail.com>
2015-04-14 21:10:54 -0700

    Built basic character control and environment
    We put the character in with the proper scripts. Check for camera
    problems, if any.

--- Assets/Scripts/SpringTrigger.cs
+++ Assets/Scripts/SpringTrigger.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class SpringTrigger : MonoBehaviour {
+	
 	Animator anim;
-	int springHash = Animator.StringToHash("hinge");
+	GameObject top;
 
 	
 	public GameObject tong; 
@@ -14,18 +15,14 @@ public class SpringTrigger : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetTrigger (springHash);
-        }
-		
-	
+
 	}
 
 	void OnTriggerEnter (Collider cubeTrigger)
 	{    
-		
+		if (cubeTrigger.tag == "Tongs")
+		{
+			anim.enabled = !anim.enabled;
 	}
 	
 }
