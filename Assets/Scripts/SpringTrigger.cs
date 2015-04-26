0f4ed0f4c4dc8434ccea0129f794bd99b010aba0
Haub100 <haub100@mail.chapman.edu>
2015-04-26 11:50:32 -0700

    Springtrig

--- Assets/Scripts/SpringTrigger.cs
+++ Assets/Scripts/SpringTrigger.cs
@@ -2,28 +2,34 @@
 using System.Collections;
 
 public class SpringTrigger : MonoBehaviour {
-	
+
+  public float JumpMagnitude = 20;
 	Animator anim;
 	GameObject top;
 
 	
-	public GameObject tong; 
-	// Use this for initialization
-	void Start () {
-		anim = GetComponent<Animator>();
-	}
+  //public GameObject tong; 
+  //// Use this for initialization
+  //void Start () {
+  //  anim = GetComponent<Animator>();
+  //}
+
+  public void ControllerEnter2D(CharacterController2D controller)
+  {
+    controller.SetVerticalForce(JumpMagnitude);
+  }
 	
-	// Update is called once per frame
-	void Update () {
+  //// Update is called once per frame
+  //void Update () {
 
-	}
+  //}
 
-	void OnTriggerEnter (Collider cubeTrigger)
-	{    
-		if (cubeTrigger.tag == "Tongs")
-		{
-			anim.enabled = !anim.enabled;
-		}
-	}
+  //void OnTriggerEnter(Collider cubeTrigger)
+  //{
+  //  if (cubeTrigger.tag == "Tongs")
+  //  {
+  //    anim.enabled = !anim.enabled;
+  //  }
+  //}
 	
 }
